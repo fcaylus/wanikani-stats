0fd0e88700bd7d86787221e805dbe6e638f860ea
Fabien Caylus <fabien.caylus@gmail.com>
2020-03-02 00:52:27 +0900

    Add github repo to the footer
    Signed-off-by: Fabien Caylus <fabien.caylus@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
         appName: "WaniKani Stats Reloaded",
         author: "Fabien Caylus",
         authorLink: "https://github.com/fcaylus",
+        githubLink: "https://github.com/fcaylus/wanikani-stats",
         defaultPort: "3170"
     }
 };
--- src/app/components/PageFooter.tsx
+++ src/app/components/PageFooter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import {Box} from "@material-ui/core";
@@ -10,21 +10,29 @@ export default function PageFooter() {
     return (
         <Box m={3} component="footer">
             <Typography variant="body2" color="textSecondary" align="center">
-                {'Made by '}
-                <Link color="inherit" href={process.env.authorLink} target="_blank">
+                {"Made by "}
+                <Link href={process.env.authorLink} target="_blank">
                     {process.env.author}
-                </Link>{' '}
-                {'in \u{1f5fc} Tokyo \u{1f5fc} !'}
+                </Link>
+                {" in \u{1f5fc} Tokyo \u{1f5fc} !"}
             </Typography>
             <Typography variant="body2" color="textSecondary" align="center">
-                {'Icon made by '}
-                <Link color="inherit" href="https://www.flaticon.com/authors/freepik" target="_blank">
+                {"Want to checkout the source code ? Check the "}
+                <Link href={process.env.githubLink} target="_blank">
+                    project
+                </Link>
+                {" on GitHub."}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" align="center">
+                {"Icon made by "}
+                <Link href="https://www.flaticon.com/authors/freepik" target="_blank">
                     Freepik
                 </Link>
-                {' from '}
-                <Link color="inherit" href="https://www.flaticon.com/" target="_blank">
+                {" from "}
+                <Link href="https://www.flaticon.com/" target="_blank">
                     flaticon.com
                 </Link>
+                .
             </Typography>
         </Box>
     );
