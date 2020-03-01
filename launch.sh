4a0a3b629819c09ee424b72325a25fa18e011b78
Fabien Caylus <fabien.caylus@gmail.com>
2020-03-02 00:19:50 +0900

    Update launch.sh script
    Signed-off-by: Fabien Caylus <fabien.caylus@gmail.com>

--- launch.sh
+++ launch.sh
@@ -3,15 +3,21 @@
 ###
 
 # Extract the archive
+echo "Extracting archive ..."
 tar -xzf ./wanikani-stats_docker.tar.gz
 
 # Stop the previous container
-sudo docker stop wanikani-stats 
+echo "Stopping and removing previous container/image ..."
+sudo docker stop wanikani-stats
 sudo docker rm wanikani-stats
 sudo docker rmi wanikani-stats/server:latest
 
 # Launch the docker image
+echo "Loading and starting new image/container ..."
 sudo docker load < wanikani-stats_docker.tar
 sudo docker run -td --name wanikani-stats -p 3170:3170 wanikani-stats/server
+
+echo "Restarting apache2 ..."
 sudo service apache2 restart
 
+echo "Done !"
