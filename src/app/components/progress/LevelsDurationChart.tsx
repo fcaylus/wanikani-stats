0c7c043cdb508cda5252b050811d131871cbe48d
Fabien Caylus <fabien.caylus@gmail.com>
2020-03-04 14:19:51 +0900

    Add average time on chart
    Signed-off-by: Fabien Caylus <fabien.caylus@gmail.com>

--- src/app/components/progress/LevelsDurationChart.tsx
+++ src/app/components/progress/LevelsDurationChart.tsx
@@ -1,9 +1,20 @@
 import {LevelsHashMap} from "../../../data/interfaces/level";
 import {makeStyles, Theme} from "@material-ui/core/styles";
-import {Box, Card, CardContent, CardHeader, Divider} from "@material-ui/core";
+import {Box, Card, CardContent, CardHeader, Divider, Typography} from "@material-ui/core";
 import React from "react";
-import {durationOfLevel} from "../../levels";
-import {Bar, BarChart, CartesianGrid, Cell, ResponsiveContainer, Text, Tooltip, XAxis, YAxis} from "recharts";
+import {averageLevelDuration, durationOfLevel} from "../../levels";
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Cell,
+    ReferenceLine,
+    ResponsiveContainer,
+    Text,
+    Tooltip,
+    XAxis,
+    YAxis
+} from "recharts";
 import formatDuration from "../../../formatDuration";
 import graphColorForValues from "../../graphColorForValues";
 import colors from "../../colors";
@@ -25,6 +36,10 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     barLabel: {
         textAnchor: "start"
+    },
+    averageLabel: {
+        width: "99%",
+        textAlign: "end"
     }
 }));
 
@@ -86,17 +101,23 @@ export default function LevelsDurationChart(props: LevelsDurationChartProps) {
         }
 
         let data = [];
+
         for (const level of Object.values(props.levels)) {
             data.push({
                 level: level.level,
-                value: durationOfLevel(level) / DAYS_MULTIPLIER
+                value: durationOfLevel(level) / DAYS_MULTIPLIER,
             });
         }
         return data;
     };
 
+    if (!props.levels) {
+        return null;
+    }
+
     const data = generateGraphData();
     const colors = graphColorForValues(data.map((item) => item.value));
+    const average = averageLevelDuration(Object.values(props.levels)) / DAYS_MULTIPLIER;
 
     // 60 is approximately the size of the left axis
     // 20 is the min size of a bar
@@ -108,51 +129,56 @@ export default function LevelsDurationChart(props: LevelsDurationChartProps) {
             <CardHeader title="Time on Levels"/>
             <Divider/>
             <CardContent className={classes.container}>
-                {props.levels && (
-                    <Box className={classes.box} style={{
-                        minWidth: minWidth + theme.spacing(1) * 2
-                    }}>
-                        <ResponsiveContainer height={GRAPH_HEIGHT} width={"99%"} minWidth={minWidth}
-                                             className={classes.content}>
-                            <BarChart data={data} margin={{
-                                top: 0,
-                                bottom: 0,
-                                left: 0,
-                                right: 0
-                            }}>
-                                <CartesianGrid vertical={false}/>
-                                <XAxis dataKey="level" label={{
-                                    value: "Levels",
-                                    position: "bottom",
-                                }}/>
-                                <YAxis label={{
-                                    value: "Time on Level (days)",
-                                    angle: -90,
-                                    position: "insideLeft",
-                                    offset: 10,
-                                    style: {
-                                        textAnchor: "middle",
-                                    }
-                                }}/>
+                <Box className={classes.box} style={{
+                    minWidth: minWidth + theme.spacing(1) * 2
+                }}>
+                    <Typography variant="body2" className={classes.averageLabel}>
+                        <b>Average time: </b>
+                        {formatDuration(average * DAYS_MULTIPLIER)}
+                    </Typography>
+                    <ResponsiveContainer height={GRAPH_HEIGHT} width={"99%"} minWidth={minWidth}
+                                         className={classes.content}>
+                        <BarChart data={data} margin={{
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0
+                        }}>
+                            <CartesianGrid vertical={false}/>
+                            <XAxis dataKey="level" label={{
+                                value: "Levels",
+                                position: "bottom",
+                            }}/>
+                            <YAxis label={{
+                                value: "Time on Level (days)",
+                                angle: -90,
+                                position: "insideLeft",
+                                offset: 10,
+                                style: {
+                                    textAnchor: "middle",
+                                }
+                            }}/>
 
-                                <Tooltip
-                                    formatter={(value: any) => {
-                                        return [formatDuration(value * DAYS_MULTIPLIER), "Time on level"]
-                                    }}
-                                    labelFormatter={(value: any) => {
-                                        return "Level " + value;
-                                    }}/>
-                                <Bar dataKey="value" label={<BarLabel/>}>
-                                    {
-                                        data.map((entry, index) => {
-                                            return <Cell key={index} fill={colors[entry.value]}/>;
-                                        })
-                                    }
-                                </Bar>
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </Box>
-                )}
+                            <Tooltip
+                                formatter={(value: any) => {
+                                    return [formatDuration(value * DAYS_MULTIPLIER), "Time on level"]
+                                }}
+                                labelFormatter={(value: any) => {
+                                    return "Level " + value;
+                                }}/>
+                            <ReferenceLine y={average} ifOverflow="extendDomain" isFront={false}
+                                           stroke={theme.palette.primary.light} strokeDasharray="5 4"
+                                           strokeWidth={2}/>
+                            <Bar dataKey="value" label={<BarLabel/>}>
+                                {
+                                    data.map((entry, index) => {
+                                        return <Cell key={index} fill={colors[entry.value]}/>;
+                                    })
+                                }
+                            </Bar>
+                        </BarChart>
+                    </ResponsiveContainer>
+                </Box>
             </CardContent>
         </Card>
     )
